84b86a44e132a42b52c6792c953e63d34a96cb98
siriusmd99 <siriusmd99@gmail.com>
2018-11-04 23:50:34 +0200

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "iterations"
+
+void sample()
+{
+   //Nothing, we need just the function call here...
+}
+
+int main()
+{
+    // Reseting counter to 0
+	reset_ticks();
+
+	//Using our code below
+	for(int i = 0; i < 1000; i++)
+    {
+        1+5;
+    }
+
+    sample();
+    sample();
+    sample();
+
+    //Getting Number of Iterations after our code gets executed
+    tick_int result = get_ticks();
+
+    //Printing result
+    cout << "Iterations : " << result << endl;
+
+    return 0;
+}
+
+
